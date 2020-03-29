34f92c76e9f90fc3cf0271e462b1d03c3575cde1
Abdou Soumah <abdou.soumah@gmail.com>
2020-03-29 14:47:31 +0200

    version published fix

--- pages/_conferenceId/index.vue
+++ pages/_conferenceId/index.vue
@@ -16,7 +16,7 @@
 export default {
   asyncData(context) {
     return context.app.$storyapi.get(`cdn/stories/conference/${context.params.conferenceId}`, {
-      version: 'draft'
+      version: process.env.NODE_ENV == "production" ? "published" : "draft"
     }).then(res => {
       return {
         blok: res.data.story.content,
